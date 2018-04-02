587687bdc6bd3d22388c32d084fcb42dad278224
drek226 <37354452+drek226@users.noreply.github.com>
2018-04-01 21:10:23 -0500

    Final for objects and strings

--- src/P2_BananaStand.java
+++ src/P2_BananaStand.java
@@ -7,7 +7,24 @@
 
 public class P2_BananaStand {
 	public static void main(String[] args) {
-		String quote = "There's always money in the banana stand.";
+		
+			
+				String quote = "BANANA.";
+				System.out.println(quote);
+				
+				{
+				      String s1 = ("There's always money in the banana stand.");
+				      System.out.println(s1.substring(21));
+	
+	}
+				
+			
+
+
+					
+				
+			}
+		
 		
 }
-}
+
--- src/P4_Vertical.java
+++ src/P4_Vertical.java
@@ -4,10 +4,17 @@
 	Date: 4/1/18
 */
 
-public class P4_Vertical {
 
-	public static void main(String[] args) {
 
-	}
 
+public static void main(String[] args) {
+    vertical()
+}
+
+public static void vertical(String str) {
+    String vertical = "hey now";
+    for (int i = 0; i < str.length(); i++) {
+        System.out.println(str.charAt(i));
+    
+} 
 }
